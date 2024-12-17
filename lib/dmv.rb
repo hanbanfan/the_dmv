ecebd202514316a544c6d2b5646e422884b1ddc7
Hannah Gray <hannah7gray@gmail.com>
2024-12-17 16:24:49 -0700

    Fix Dmv class implementation and update tests

--- lib/dmv.rb
+++ lib/dmv.rb
@@ -1,16 +1,23 @@
+# The Dmv class represents the DMV system that manages facilities.
 class Dmv
+  attr_reader :facilities
 
+  # Initializes an empty array to store Facility objects.
   def initialize
     @facilities = []
   end
 
+  # Adds a facility to the DMV.
+  # @param facility [Facility] The facility to add.
   def add_facility(facility)
     @facilities << facility
   end
 
+  # Returns facilities that offer a specific service.
+  # @param service [String] The service name to search for.
+  # @return [Array<Facility>] List of facilities providing the service.
   def facilities_offering_service(service)
-    @facilities.find do |facility|
-      facility.services.include?(service)
-    end
+    @facilities.select { |facility| facility.services.include?(service) }
   end
 end
+
--- spec/dmv_spec.rb
+++ spec/dmv_spec.rb
@@ -1,43 +1,38 @@
-require 'spec_helper'
+require 'rspec'
+require './lib/dmv'
 
 RSpec.describe Dmv do
   before(:each) do
-    @dmv = Dmv.new
-    @facility_1 = Facility.new({name: 'DMV Tremont Branch', address: '2855 Tremont Place Suite 118 Denver CO 80205', phone: '(720) 865-4600'})
-    @facility_2 = Facility.new({name: 'DMV Northeast Branch', address: '4685 Peoria Street Suite 101 Denver CO 80239', phone: '(720) 865-4600'})
-    @facility_3 = Facility.new({name: 'DMV Northwest Branch', address: '3698 W. 44th Avenue Denver CO 80211', phone: '(720) 865-4600'})
+    @dmv = Dmv.new # Creates a new Dmv instance before each test
   end
 
-  describe '#initialize' do
-    it 'can initialize' do
-      expect(@dmv).to be_an_instance_of(Dmv)
-      expect(@dmv.facilities).to eq([])
-    end
+  it 'can initialize' do
+    expect(@dmv).to be_a(Dmv) # Checks that @dmv is an instance of Dmv
+    expect(@dmv.facilities).to eq([]) # Facilities array should start empty
   end
 
-  describe '#add facilities' do
-    it 'can add available facilities' do
-      expect(@dmv.facilities).to eq([])
-      @dmv.add_facility(@facility_1)
-      expect(@dmv.facilities).to eq([@facility_1])
-    end
+  it 'can add facilities' do
+    mock_facility_1 = double('Facility', services: ['Renew License'])
+    mock_facility_2 = double('Facility', services: ['Vehicle Registration'])
+
+    @dmv.add_facility(mock_facility_1)
+    @dmv.add_facility(mock_facility_2)
+
+    expect(@dmv.facilities).to include(mock_facility_1)
+    expect(@dmv.facilities).to include(mock_facility_2)
   end
 
-  describe '#facilities_offering_service' do
-    it 'can return list of facilities offering a specified Service' do
-      @facility_1.add_service('New Drivers License')
-      @facility_1.add_service('Renew Drivers License')
-      @facility_2.add_service('New Drivers License')
-      @facility_2.add_service('Road Test')
-      @facility_2.add_service('Written Test')
-      @facility_3.add_service('New Drivers License')
-      @facility_3.add_service('Road Test')
+  it 'can return facilities offering a specified service' do
+    facility_1 = double('Facility', services: ['Renew License'])
+    facility_2 = double('Facility', services: ['Vehicle Registration'])
+    facility_3 = double('Facility', services: ['Renew License', 'Road Test'])
 
-      @dmv.add_facility(@facility_1)
-      @dmv.add_facility(@facility_2)
-      @dmv.add_facility(@facility_3)
+    @dmv.add_facility(facility_1)
+    @dmv.add_facility(facility_2)
+    @dmv.add_facility(facility_3)
 
-      expect(@dmv.facilities_offering_service('Road Test')).to eq([@facility_2, @facility_3])
-    end
+    result = @dmv.facilities_offering_service('Renew License')
+    expect(result).to eq([facility_1, facility_3])
   end
 end
+
