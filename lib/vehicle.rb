7f805f4befc769881a5bac9a10254d661eccaf1e
Hannah Gray <hannah7gray@gmail.com>
2024-12-17 16:57:49 -0700

    updated vehicle class

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,37 +1,20 @@
-require 'date'
+require_relative 'vehicle'
 
-# The Vehicle class represents a registered vehicle.
-class Vehicle
-  attr_reader :vin, :year, :make, :model, :engine, :registration_date, :plate_type
-
-  def initialize(details)
-    @vin = details[:vin]
-    @year = details[:year]
-    @make = details[:make]
-    @model = details[:model]
-    @engine = details[:engine]
-    @registration_date = nil
-    @plate_type = nil
-  end
-
-  # Registers the vehicle and determines the plate type.
-  def register
-    @registration_date = Date.today
-    @plate_type = determine_plate_type
-  end
-
-  # Determines plate type.
-  def determine_plate_type
-    return :antique if antique?
-    return :ev if electric?
-    :regular
-  end
-
-  def antique?
-    Date.today.year - @year > 25
-  end
-
-  def electric?
-    @engine == :ev
+# The VehicleFactory class creates Vehicle objects from external data sources.
+class VehicleFactory
+  # Creates an array of Vehicle objects from external data.
+  # @param data [Array<Hash>] An array of vehicle details from the API.
+  # @return [Array<Vehicle>] An array of Vehicle objects.
+  def create_vehicles(data)
+    data.map do |vehicle_data|
+      Vehicle.new({
+        vin: vehicle_data[:vin_1_10],
+        year: vehicle_data[:model_year].to_i,
+        make: vehicle_data[:make],
+        model: vehicle_data[:model],
+        engine: :ev # Since data is for EV registrations, set engine type to :ev
+      })
+    end
   end
 end
+
