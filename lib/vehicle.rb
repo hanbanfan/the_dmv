8ac50acf74fc9a1e820fa79a5262c79b8e78ccad
Hannah Gray <hannah7gray@gmail.com>
2024-12-17 16:43:21 -0700

    Add Vehicle class with registration and plate determination

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,51 +1,37 @@
 require 'date'
 
-# The Vehicle class represents a registered vehicle at the DMV.
+# The Vehicle class represents a registered vehicle.
 class Vehicle
-  # Readable attributes for vehicle details and registration
   attr_reader :vin, :year, :make, :model, :engine, :registration_date, :plate_type
 
-  # Initializes a Vehicle object with provided details
-  # @param details: Hash containing vehicle attributes
   def initialize(details)
-    @vin = details[:vin]          # Vehicle Identification Number
-    @year = details[:year]        # Year of manufacture
-    @make = details[:make]        # Manufacturer name
-    @model = details[:model]      # Model of the vehicle
-    @engine = details[:engine]    # Engine type (e.g., :ice, :ev)
-    @registration_date = nil      # Default: vehicle is not registered
-    @plate_type = nil             # Default: no plate type assigned
+    @vin = details[:vin]
+    @year = details[:year]
+    @make = details[:make]
+    @model = details[:model]
+    @engine = details[:engine]
+    @registration_date = nil
+    @plate_type = nil
   end
 
-  # Registers the vehicle
-  # Sets the registration date to today's date and determines the plate type
+  # Registers the vehicle and determines the plate type.
   def register
     @registration_date = Date.today
     @plate_type = determine_plate_type
   end
 
-  # Determines the plate type based on vehicle age and engine type
-  # @return: Symbol representing plate type (:antique, :ev, or :regular)
+  # Determines plate type.
   def determine_plate_type
-    if antique?
-      :antique
-    elsif electric?
-      :ev
-    else
-      :regular
-    end
+    return :antique if antique?
+    return :ev if electric?
+    :regular
   end
 
-  # Checks if the vehicle is antique (older than 25 years)
-  # @return: Boolean indicating if the vehicle is antique
   def antique?
     Date.today.year - @year > 25
   end
 
-  # Checks if the vehicle is electric
-  # @return: Boolean indicating if the vehicle is electric
   def electric?
     @engine == :ev
   end
 end
-
