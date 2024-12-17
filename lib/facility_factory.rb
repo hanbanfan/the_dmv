60784804e8914205fc5839a61784ec2643cef220
Hannah Gray <hannah7gray@gmail.com>
2024-12-17 16:52:54 -0700

    added facility factory

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -1,15 +1,15 @@
 require_relative 'facility'
 
-# The FacilityFactory class is responsible for creating Facility objects from external data.
+# The FacilityFactory class creates Facility objects from external data sources.
 class FacilityFactory
-  # Creates Facility objects from provided data
-  #  Array of hashes containing facility details
-  # @return: Array of Facility objects
+  # Creates an array of Facility objects from external data.
+  # @param data [Array<Hash>] An array of facility details from the API.
+  # @return [Array<Facility>] An array of Facility objects.
   def create_facilities(data)
     data.map do |facility_data|
       Facility.new({
         name: facility_data[:dmv_office],
-        address: facility_data[:address_li],
+        address: "#{facility_data[:address_li]}, #{facility_data[:city]}",
         phone: facility_data[:phone]
       })
     end
