e6c34b77ce5892ba442344dd27df50d66f56f1d5
Hannah Gray <hannah7gray@gmail.com>
2024-12-17 17:29:45 -0700

    Update Dmv spec to use updated Facility initialization

--- spec/dmv_spec.rb
+++ spec/dmv_spec.rb
@@ -1,43 +1,30 @@
-require 'spec_helper'
-
 RSpec.describe Dmv do
   before(:each) do
     @dmv = Dmv.new
-    @facility_1 = Facility.new({name: 'DMV Tremont Branch', address: '2855 Tremont Place Suite 118 Denver CO 80205', phone: '(720) 865-4600'})
-    @facility_2 = Facility.new({name: 'DMV Northeast Branch', address: '4685 Peoria Street Suite 101 Denver CO 80239', phone: '(720) 865-4600'})
-    @facility_3 = Facility.new({name: 'DMV Northwest Branch', address: '3698 W. 44th Avenue Denver CO 80211', phone: '(720) 865-4600'})
   end
 
-  describe '#initialize' do
-    it 'can initialize' do
-      expect(@dmv).to be_an_instance_of(Dmv)
-      expect(@dmv.facilities).to eq([])
-    end
+  it 'can initialize with no facilities' do
+    expect(@dmv.facilities).to eq([])
   end
 
-  describe '#add facilities' do
-    it 'can add available facilities' do
-      expect(@dmv.facilities).to eq([])
-      @dmv.add_facility(@facility_1)
-      expect(@dmv.facilities).to eq([@facility_1])
-    end
+  it 'can add facilities' do
+    # Pass a hash for Facility initialization
+    facility = Facility.new({ name: 'DMV Branch', address: '123 St', phone: '555-5555' })
+    @dmv.add_facility(facility)
+
+    expect(@dmv.facilities).to include(facility)
   end
 
-  describe '#facilities_offering_service' do
-    it 'can return list of facilities offering a specified Service' do
-      @facility_1.add_service('New Drivers License')
-      @facility_1.add_service('Renew Drivers License')
-      @facility_2.add_service('New Drivers License')
-      @facility_2.add_service('Road Test')
-      @facility_2.add_service('Written Test')
-      @facility_3.add_service('New Drivers License')
-      @facility_3.add_service('Road Test')
+  it 'can find facilities offering specific services' do
+    facility_1 = Facility.new({ name: 'DMV 1', address: '123 St', phone: '555-5551' })
+    facility_2 = Facility.new({ name: 'DMV 2', address: '456 St', phone: '555-5552' })
+    facility_1.add_service('Vehicle Registration')
+    facility_2.add_service('Renew License')
 
-      @dmv.add_facility(@facility_1)
-      @dmv.add_facility(@facility_2)
-      @dmv.add_facility(@facility_3)
+    @dmv.add_facility(facility_1)
+    @dmv.add_facility(facility_2)
 
-      expect(@dmv.facilities_offering_service('Road Test')).to eq([@facility_2, @facility_3])
-    end
+    expect(@dmv.facilities_offering_service('Vehicle Registration')).to eq([facility_1])
+    expect(@dmv.facilities_offering_service('Renew License')).to eq([facility_2])
   end
 end
